ed933b9014d8a6617742deaeb35f8024b2303e9b
Flaminghurricane <86059679+Flaminghurricane@users.noreply.github.com>
2021-07-05 18:59:17 +0530

    Add files via upload

---
+++ Player.js
@@ -0,0 +1,62 @@
+class Player
+{
+    constructor() {
+		this.x = 500;
+		this.y=200;
+		this.spt=createSprite(this.x, this.y, 100,100);
+		this.spt.shapeColor="blue";
+
+	}
+
+  applyGravity()
+    {
+        this.spt.velocityY=this.spt.velocityY+2;
+	}
+	
+
+    moveleft(){ 
+
+       this.spt.x += -12;
+
+	}
+
+moveright(){ 
+
+   this.spt.x += 12;
+
+}
+
+jump(){ 
+
+this.spt.velocityY = -12;
+
+}
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
